91795a8f091eeb87c83f784a9251b0fa8b2aeb8b
Cindy <759501948@qq.com>
2017-02-15 11:54:35 +0800

    SharedPreferences;读取全局可读文件。

--- app/src/main/java/com/example/admin/login/MainActivity.java
+++ app/src/main/java/com/example/admin/login/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.admin.login;
 
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
@@ -20,9 +22,13 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+@SuppressLint("WorldReadableFiles")
 public class MainActivity extends AppCompatActivity {
     private EditText et_name,et_key;
     private TextView tv_sdcard;
+    private FileOutputStream fileOutputStream;
+    private FileInputStream fileInputStream;
+    private SharedPreferences sp;
 
     File path = Environment.getExternalStorageDirectory();
 
@@ -37,36 +43,33 @@ public class MainActivity extends AppCompatActivity {
         readAccount();
         sdcard();
     }
+
 //获取SD卡剩余容量
     private void sdcard() {
         StatFs statfs = new StatFs(path.getPath());
         long blockSize;
-        long totalBlocks;
         long availableBlocks;
         //获取当前系统版本的等级
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2){
             blockSize = statfs.getBlockSizeLong();
-            totalBlocks = statfs.getBlockCountLong();
             availableBlocks = statfs.getAvailableBlocksLong();
         }else {
             blockSize = statfs.getBlockSize();
-            totalBlocks = statfs.getBlockCount();
             availableBlocks = statfs.getAvailableBlocks();
         }
         tv_sdcard = (TextView) findViewById(R.id.tv_sdcard);
+//        区块大小(totalBlocks = stat.getBlockCountLong();) * 区块数量 等于 存储设备的总大小
         tv_sdcard.setText("  sd卡剩余空间："+formatSize(availableBlocks * blockSize));
 
     }
     private String formatSize(long size){
         return Formatter.formatFileSize(this,size);
     }
-
+    //读写文件
     public void login(View view){
         String name = et_name.getText().toString();
         String key = et_key.getText().toString();
-
         CheckBox checkBox = (CheckBox) findViewById(R.id.cb_remember);
-
         if (checkBox.isChecked()){
 //            File file =new File("data/data/com.example.admin.login/info.txt");
 //            用API在内部读写文件
@@ -76,10 +79,10 @@ public class MainActivity extends AppCompatActivity {
             //MEDIA_UNMOUNTED:sd卡存在但是没有挂载
             //MEDIA_CHECKING:sd卡正在准备
             //MEDIA_MOUNTED：sd卡已经挂载，可用
-            if (path.equals(Environment.MEDIA_MOUNTED)){
+            /**if (path.equals(Environment.MEDIA_MOUNTED)){
                 //返回一个File对象，其路径是sd卡的真实路径
                 File file = new File(path,"info.txt");
-                FileOutputStream fileOutputStream;
+
             try {
                 fileOutputStream = new FileOutputStream(file);
                 fileOutputStream.write((name+"##"+key).getBytes());
@@ -89,7 +92,14 @@ public class MainActivity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
+        }*/
+//        SharedPreferences
+            sp = getSharedPreferences("info",MODE_PRIVATE);
+            SharedPreferences.Editor ed = sp.edit();
+            ed.putString("name",name);
+            ed.putString("key",key);
+            ed.commit();
+
         Toast.makeText(this, "Login succeed", Toast.LENGTH_SHORT).show();
      }
     }
@@ -99,17 +109,16 @@ public class MainActivity extends AppCompatActivity {
 //        File file = new File(getCacheDir(),"info.txt");
 //        外部存储
         //MEDIA_MOUNTED：sd卡已经挂载，可用
-        if (path.equals(Environment.MEDIA_MOUNTED)) {
+       /** if (path.equals(Environment.MEDIA_MOUNTED)) {
             File file = new File("sdcard/info.txt");
             if (file.exists()) {
                 try {
-                    FileInputStream fileInputStream = new FileInputStream(file);
+                    fileInputStream = new FileInputStream(file);
                     //把字节流转换成字符流
                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
                     //读取txt文件里的用户名和密码
                     String text = bufferedReader.readLine();
                     String[] strings = text.split("##");
-
                     et_name.setText(strings[0]);
                     et_key.setText(strings[1]);
                 } catch (FileNotFoundException e) {
@@ -118,6 +127,54 @@ public class MainActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
             }
+        }*/
+//        SharedPreferences
+        sp = getSharedPreferences("info",MODE_PRIVATE);
+        String name = sp.getString("name","");
+        String key = sp.getString("key","");
+        et_name.setText(name);
+        et_key.setText(key);
+    }
+    //文件访问权限
+    public void create1(View view){
+        try {
+            fileOutputStream = openFileOutput("info1.txt",MODE_PRIVATE);
+            try {
+                fileOutputStream.write("hello".getBytes());
+                fileOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
+    }
+    public void create2(View view) {
+        try {
+//                    @SuppressWarnings("deprecation")
+                    fileOutputStream = openFileOutput("info1.txt", MODE_WORLD_READABLE | MODE_WORLD_WRITEABLE);
+            try {
+                fileOutputStream.write("hehe".getBytes());
+                fileOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    //读取全局可读的文件
+    public void click(View view){
+        File file =new File("data/data/com.example.admin.login/info.txt");
+        try {
+            fileInputStream = new FileInputStream(file);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+            Toast.makeText(this, bufferedReader.readLine(), Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
